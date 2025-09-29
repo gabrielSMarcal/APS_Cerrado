d8835947712b7dd8f79ebff849f6be96debbf6b9
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-09-29 20:32:33 -0300

    first commit

---
+++ data/fonte.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv('queimadas_cerrado_01.csv')
+
+df = df.drop(['Satelite', 'Pais', 'Municipio', 'Bioma'], axis=1)
+
+df['DataHora'] = pd.to_datetime(df['DataHora']).dt.date
+
+print(df.head())
