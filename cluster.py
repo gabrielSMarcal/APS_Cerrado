93ebad74347b5490db30695af69c86e3c57d0c6b
gabrielSMarcal <gabriel.santos.marcal1@gmail.com>
2025-10-25 14:25:13 -0300

    feat: enhance data processing in cluster module with date conversion and month extraction

--- cluster.py
+++ cluster.py
@@ -1,5 +1,28 @@
+import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import seaborn as sns
+from sklearn.cluster import KMeans
+
+from sklearn.metrics import silhouette_samples
+
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import OneHotEncoder
+
 from data import connection as c
 
 df = c.connection()
 
-print(df.head())
+# Converter para data
+df['Data'] = pd.to_datetime(df['Data'])
+
+# Extrair o mês
+df['Mes'] = df['Data'].dt.month
+
+for mes in range (1, 13):
+    df[f'Mes_{mes}'] = (df['Mes'] == mes).astype(int)
+        
+# Remover o mês
+df = df.drop(columns=['Mes'])
+
